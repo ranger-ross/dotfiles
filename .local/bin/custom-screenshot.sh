f8a178086ca591dc0fcba2837691c121f00b8e12
Ross Sullivan <rosssullivan101@gmail.com>
2026-06-13 12:49:28 +0900

    disabled screenshot notification

--- .local/bin/custom-screenshot.sh
+++ .local/bin/custom-screenshot.sh
@@ -1,1 +1,1 @@
-grim -t ppm -g "$(slurp -d -w 2)" - | satty --filename - --output-filename ~/Pictures/Screenshots/Screenshot_$time.png --early-exit --actions-on-enter save-to-clipboard --copy-command 'wl-copy'
+grim -t ppm -g "$(slurp -d -w 2)" - | satty --filename - --output-filename ~/Pictures/Screenshots/Screenshot_$time.png --early-exit --actions-on-enter save-to-clipboard --disable-notifications --copy-command 'wl-copy'
