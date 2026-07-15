9a6edacc5be40f50b1feeeb0586699c1792b1d95
Ross Sullivan <rosssullivan101@gmail.com>
2026-07-15 21:39:08 +0900

    added markdown preview nvim plugin

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -200,4 +200,12 @@ return {
     ---@type quicker.SetupOptions
     opts = {},
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
 }
