c5529481f72bd6e98bce82f425dadade10d9e013
Ross Sullivan <rosssullivan101@gmail.com>
2026-07-19 14:51:14 +0900

    use biome for formatting js

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -636,6 +636,11 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+
+        javascript = { 'biome' },
+        typescript = { 'biome' },
+        javascriptreact = { 'biome' },
+        typescriptreact = { 'biome' },
       },
     },
   },
