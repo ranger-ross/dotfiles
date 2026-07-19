56e9d24b847fd9ed3fffdbd53a4db5ea3e69b4e4
Ross Sullivan <rosssullivan101@gmail.com>
2026-07-19 14:58:40 +0900

    removed inlay hints from javascript

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -537,30 +537,6 @@ require('lazy').setup({
             'javascriptreact',
             'javascript.jsx',
           },
-          settings = {
-            typescript = {
-              inlayHints = {
-                includeInlayParameterNameHints = 'all',
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-            javascript = {
-              inlayHints = {
-                includeInlayParameterNameHints = 'all',
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-          },
         },
         tailwindcss = {},
       }
